e878672d581161d51d19ad0e4bcbea5f5e22f3fb
Katinkabeat <tracey8008@hotmail.com>
2026-05-21 15:18:10 -0300

    Wrap app in SQErrorBoundary; bump cache v32 (c117)

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { SQErrorBoundary } from '../../rae-side-quest/packages/sq-ui/index.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SQErrorBoundary label="rungles">
+      <App />
+    </SQErrorBoundary>
   </React.StrictMode>
 )
 
